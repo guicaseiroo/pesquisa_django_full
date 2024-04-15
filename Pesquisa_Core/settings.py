f944bdb835d518ca5daadcf737b99b60e5f614f2
guicaseiroo <caseiro.gui@gmail.com>
2024-04-15 00:09:18 -0300

    server config

--- Pesquisa_Core/settings.py
+++ Pesquisa_Core/settings.py
@@ -137,8 +137,11 @@ USE_THOUSAND_SEPARATOR=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/root/pesquisa_django_full/static/'
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = '/root/pesquisa_django_full/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
@@ -148,5 +151,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/root/pesquisa_django_full/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/root/pesquisa_django_full/media' 
